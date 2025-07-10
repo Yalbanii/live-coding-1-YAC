2f2031084b50f991a842411c297535a0f9a46881
Yalbani Aranda <yarandacardona@gmail.com>
2025-07-10 11:15:01 -0600

    ejercio

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
-// Escribe tu código aquí.
+// Ejercicio 2
+
+function imprimirLista(arrayMiLista){
+ const lista = document.getElementById("miLista");
+
+arrayMiLista.forEach(function(elemento){
+ const elementoLista = document.createElement("li");
+elementoLista.textContent = elemento;
+ lista.appendChild(elementoLista);
+ });
+}
+const myArray = ['insecto', 'bootcamp', 'escritorio', 'manzana'];
+imprimirLista(myArray);
+myArray.push("manzana");
+
+// Ejercicio 1
+function listaPalabras (parDos, parUno){
+for (i = 0 ; i <= 5; i++){
+ if (parDos[i].length > parUno.length){
+   console.log(parDos[i]);
+}
+ }
+}
+const parDos = ['insecto', 'bootcamp', 'mangos', 'reptil', 'mosca', 'escritorio'];
+const parUno = ("bocina");
+console.log(listaPalabras(parDos, parUno));
